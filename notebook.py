6c91249fc26671b096d896c1bee6dffeeb92b5fd
skyxie <skyxie>
2022-09-15 19:56:59 +0000

    Trim notebook
    down to what's needed for a test

--- notebook.py
+++ notebook.py
@@ -1,10 +1,4 @@
 # Databricks notebook source
-
-# MAGIC %load_ext autoreload
-# MAGIC %autoreload 2
-
-# COMMAND ----------
-
 import sys
 from pyspark.sql import functions as F
 from pyspark.sql import types as T
@@ -24,22 +18,3 @@ df.show()
 
 plus_one_udf = F.udf(plus_one, T.IntegerType())
 df.withColumn("j", plus_one_udf("i")).show()
-
-# COMMAND ----------
-
-b = spark.sparkContext.broadcast(plus_one)
-
-@F.udf(returnType=T.IntegerType())
-def plus_one_broadcast_udf(a):
-    return b.value(a)
-
-df.withColumn("j", plus_one_broadcast_udf("i")).show()
-
-# COMMAND ----------
-
-def plus_one_alt(a):
-    return a + 1
-
-plus_one_alt_udf = F.udf(plus_one_alt, T.IntegerType())
-df.withColumn("j", plus_one_alt_udf("i")).show()
-
