7805236d1d43938d04ffca2e4e290534d1bbeb34
Tian <tian.xie@enigma.com>
2022-09-21 12:56:06 -0400

    Exclude more from package

--- setup.py
+++ setup.py
@@ -9,5 +9,10 @@ setup(
     author="Machine Learning Engineering",
     author_email="ml_engineering_team@enigma.com",
     description="Python library to test UDFs in MLFlow Project.",
-    packages=find_packages(exclude="mlflow_project_with_package"),
+    packages=find_packages(
+        exclude=[
+            "mlflow_project_with_package",
+            "mlflow_project_alt"
+        ]
+    ),
 )
