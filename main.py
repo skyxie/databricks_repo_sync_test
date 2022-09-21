435765188e2ec4724df27a46cf253b70a5bc0926
Tian <tian.xie@enigma.com>
2022-09-21 10:12:46 -0400

    Add test case with MLFlow Project

---
+++ main.py
@@ -0,0 +1,25 @@
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
+from pyspark.sql import SparkSession
+
+from databricks_repo_sync_test import plus_one
+import mlflow
+
+
+def main():
+    spark = SparkSession.builder.getOrCreate()
+    df = spark.createDataFrame([(0,),(1,)], ("i",))
+    plus_one_udf = F.udf(plus_one, T.IntegerType())
+    df.withColumn("j", plus_one_udf("i")).show()
+    result = df.select(
+        F.avg("j").alias("avg"),
+        F.max("j").alias("max"),
+        F.min("j").alias("min"),
+    ).collect()[0]
+    mlflow.log_metric("j_avg", result.avg)
+    mlflow.log_metric("j_min", result.min)
+    mlflow.log_metric("j_max", result.max)
+
+
+if __name__ == "__main__":
+    main()
